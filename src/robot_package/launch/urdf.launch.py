9860d95f10445b5549fc5962f57d79831a1c6204
ddugas5 <ddugashf1@gmail.com>
2024-11-01 13:45:08 -0500

    simulated a box rviz using urdf

---
+++ src/robot_package/launch/urdf.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Path to the URDF file
+    urdf_file = os.path.join(
+        os.path.expanduser('~'),
+        'ros2-learning-portfolio',
+        'src',
+        'robot_package',
+        'urdf',
+        'my_robot.urdf'
+    )
+
+    # Load the URDF file
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_file).read()}],
+            remappings=[('/robot_description', '/robot_description')]
+        )
+    ])
