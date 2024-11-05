3e1e1b1d2c71e0c20f749dcdc13162a536289508
ddugas5 <ddugashf1@gmail.com>
2024-11-05 12:51:01 -0600

    added joint_state_publisher to fix wheel joint transforms

--- install/robot_package/share/robot_package/launch/two_wheel_robot.launch.py
+++ install/robot_package/share/robot_package/launch/two_wheel_robot.launch.py
@@ -24,6 +24,15 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_desc}]
-        )
+            parameters=[{'robot_description': robot_desc}],
+            remappings=[('/robot_description', '/robot_description')]
+        ),
+       Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            remappings=[('/robot_description', '/robot_description')]  # Ensure it listens to the correct topic
+
+        ),
     ])
