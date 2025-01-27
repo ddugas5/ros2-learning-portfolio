e3cccf158bc7ab5e8da0165177e9cd75c7694165
ddugas5 <ddugashf1@gmail.com>
2025-01-27 13:55:16 -0600

    velocity controller to move robot in a striaght line

---
+++ src/control_package/control_package/velocity_control.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class VelocityPublisher(Node):
+    def __init__(self):
+        super().__init__('velocity_publisher')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        twist = Twist()
+        twist.linear.x = 0.5 #move in a straight line
+        twist.angular.z = 0.0
+        self.publisher_.publish(twist)
+
+def main(args=None):
+    rclpy.init(args=args)
+    velocity_publisher = VelocityPublisher()
+    rclpy.spin(velocity_publisher)
+    velocity_publisher.destroy_node
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
